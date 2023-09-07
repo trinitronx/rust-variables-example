1ce387792df0246ad68ad8fcb07672419f8b3f00
James Cuzella <james.cuzella@lyraphase.com>
2023-09-07 12:17:53 -0600

    Add Numeric Operations demo

--- src/main.rs
+++ src/main.rs
@@ -40,5 +40,30 @@ fn main() {
     print!("Type of `y` is: ");
     print_type_of(&y);
 
+    // Numeric Operations demo
 
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("The value of `sum` is: {sum}");
+    println!("The value of `difference` is: {difference}");
+    println!("The value of `product` is: {product}");
+    println!("The value of `quotient` is: {quotient}");
+    println!("The value of `truncated` is: {truncated}");
+    print!("Type of `truncated` is: ");
+    print_type_of(&truncated);
+    println!("The value of `remainder` is: {remainder}");
 }
